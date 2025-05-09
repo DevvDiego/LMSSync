f0fe89631e73409604b40eb698c609e1699b1d1a
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-05-09 13:10:01 -0600

    Add basic svelte example

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,28 @@
 <script>
+    import Button from "./lib/components/Button.svelte";
+
 
 </script>
 
+<main class="w-dvw h-screen  text-black dark:text-white bg-neutral-500 dark:bg-neutral-900">
 
-<h1>Svelte ext</h1>
-<h1 class="text-blue-600">
-    tailwind
-</h1>
+    <div class="m-auto w-64 h-8/12 border-2 border-amber-900 text-black dark:text-white bg-neutral-500 dark:bg-neutral-900">
+        
+        <header class="w-full my-3 text-2xl text-center font-bold">
+            <p>
+                <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-white">
+                    LMS
+                </span>
+                Sync
+            </p>
+        </header>
+
+        <div>
+            <Button>
+                Button
+            </Button>
+        </div>
+
+    </div>
+
+</main>
---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+
+    let { children } = $props();
+</script>
+
+
+<button class=" min-w-4/12
+    bg-green-900 hover:bg-green-950 active:bg-black active:text-white    
+    rounded px-3 py-1 text-center
+    select-none
+    ">
+    
+    {@render children?.()}
+
+</button>
