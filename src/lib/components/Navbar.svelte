07fba142f266d84c99e1118a84bfc78bc673a14e
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-05-10 14:18:38 -0600

    Add navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+
+<nav class="bg-neutral-950">
+    <ul class="w-12/12 h-10 px-4 flex justify-between items-center">
+        <li>
+            <p class="text-xl font-semibold">
+                <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-white">
+                    LMS
+                </span>
+                Sync
+            </p>
+        </li>
+
+        <li>
+            <div class="
+                w-6 h-6 
+                flex justify-center items-center
+                rounded-full border border-green-300
+            ">
+                <span class="text-xs">A</span>
+            </div>
+        </li>
+    </ul>
+</nav>
