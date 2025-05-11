39d13aa568ca293d25e6b387ddd5c67632932f48
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-05-11 15:10:58 -0600

    Add log out function in settings page

--- src/lib/pages/protected/Settings.svelte
+++ src/lib/pages/protected/Settings.svelte
@@ -1,16 +1,29 @@
 <script>
+    import { Fetcher } from "../../classes/Fetcher";
     import Navbar from "../../components/Navbar.svelte";
+    import { logout } from "../../stores/auth.svelte";
+    import { navigateTo } from "../../stores/page.svelte";
+
+    function fetchLogout(){
+        Fetcher.fetchLogout(); //api call
+        logout(); //local state
+        navigateTo("login"); //update current page
+    }
+
 </script>
 
 <section class="w-full h-full text-center bg-neutral-900">
 
     <Navbar/>
     
-    <header class="text-md">
-        <h1>Settings page</h1>
-        
-    </header>
-
-
+    <h1 class="py-2 font-semibold text-md bg-neutral-950">Settings page</h1>
+    
+    <ul class="mt-10">
+        <li>
+            <button class="w-full p-1 bg-neutral-800" onclick={fetchLogout}>
+                Log out
+            </button>
+        </li>
+    </ul>
 
 </section>
