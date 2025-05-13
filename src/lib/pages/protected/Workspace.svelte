e46ee9849936e713412139ef23a72566701eb85e
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-05-12 22:46:01 -0600

    Enable simple request to API

--- src/lib/pages/protected/Workspace.svelte
+++ src/lib/pages/protected/Workspace.svelte
@@ -1,7 +1,38 @@
 <script>
+    import { onMount } from "svelte";
+    import Button from "../../components/Button.svelte";
     import Navbar from "../../components/Navbar.svelte";
-    import { authState, logout } from "../../stores/auth.svelte";
+    import { authState } from "../../stores/auth.svelte";
+    import { navigateTo } from "../../stores/page.svelte";
 
+    onMount(()=>{
+        //Redundancy check
+        //Add real api call for redundancy check?
+        if( !authState.isLoggedIn ){
+            navigateTo("login");
+        }
+    });
+
+    function getCourse(){
+
+        console.log("get course");
+
+    }
+
+    async function saveToAccount(){
+
+        console.log("save To Account");
+        const res = await fetch("http://localhost/www/tradex/backend/api/app.php", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }            
+        });
+        console.log( await res.text() )
+
+
+    }
 
     
 </script>
@@ -10,12 +41,22 @@
 
     <Navbar/>
     
-    <header class="text-md">
-        <h1>Status:{authState.isLoggedIn}</h1>
     
+    <h1 class="bg-neutral-950 py-2 font-semibold">
+        Workspace
+    </h1>
+    
+    <section>
+        <h1 class="py-2">Course opions</h1>
+
+        <Button onclick={getCourse}>
+            Get course
+        </Button>
         
-    </header>
+        <Button onclick={saveToAccount}>
+            Save course
+        </Button>
 
-
-
+    </section>
+    
 </section>
