c5d57d1967699a4b696ffbdc5d8b4c2b87a6d0e5
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-06-29 16:33:01 -0600

    Refactor: move scraping logic into a class named as the extension to show it interacts with the extension itself, Add: message controller to get the course info

--- src/lib/pages/protected/Workspace.svelte
+++ src/lib/pages/protected/Workspace.svelte
@@ -5,6 +5,50 @@
     import { authState } from "../../stores/auth.svelte";
     import { navigateTo } from "../../stores/page.svelte";
 
+    class LMSSync{
+
+        /**
+         * 
+         * @param {chrome.tabs.Tab} tab
+         * @throws "Page not allowed to scrape" if the page isnt a authorized page to scrape
+         */
+        static #is_valid_page(tab){
+            //find a way to not hardcode this url
+            if( !tab.url.includes("univermilenium.myopenlms.net/mod/scorm") ) throw new Error("Page not allowed to scrape");
+        }
+
+        
+        static async get_full_course(){
+
+            const tab = await LMSSync.get_current_tab();
+            LMSSync.#is_valid_page(tab);
+            
+            let response = await chrome.tabs.sendMessage( tab.id, {action: "get_course"} );
+            console.log(response);
+        }
+        
+        /**
+         * Return current active tab
+         */
+        static async get_current_tab() {
+            const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            return tab;
+        }
+        
+
+        /**
+         * 
+         * @param tab {chrome.tabs.Tab}
+        */
+        static async get_scraped_slides(tab){
+            let scraped_slides = await chrome.tabs.sendMessage( tab.id, {action: "begin_scrapping"} );
+            return scraped_slides;
+        }   
+
+
+    }
+
+
     onMount(()=>{
         //Redundancy check
         //Add real api call for redundancy check?
@@ -13,17 +57,6 @@
         }
     });
 
-
-    async function handleScrape(){
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        scraped_slides = await chrome.tabs.sendMessage( tab.id, {action: "begin_scrapping"} );
-
-        console.log(scraped_slides);
-
-    }
-
-    let scraped_slides = $state(null);
-    
 </script>
 
 <section class="w-full h-full text-center bg-neutral-900">
@@ -38,16 +71,8 @@
     <section>
         <h1 class="py-2">Course opions</h1>
 
-        <!-- <Button onclick={getCourse}>
-            Get course
-        </Button>
-        
-        <Button onclick={saveToAccount}>
+        <Button onclick={LMSSync.get_full_course}>
             Save course
-        </Button> -->
-
-        <Button onclick={handleScrape}>
-            Extraer slides
         </Button>
          
     </section>
